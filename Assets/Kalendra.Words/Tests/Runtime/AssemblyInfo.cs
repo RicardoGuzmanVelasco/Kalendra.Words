82ebc3110b58fa444437e5cbdfb43779f3504a30
RGV <ricardo.guzman.velasco@outlook.com>
2021-09-18 16:12:40 +0200

    Unity Package Layout
    Domain+Infrastructure architecture layers.
    Editor+Scene tests assemblies, respectively.
    Builders tests assembly.

---
+++ Assets/Kalendra.Words/Tests/Runtime/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("Kalendra")]
+[assembly: AssemblyProduct("Words")]
+[assembly: AssemblyTitle("Tests.Runtime")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyCopyright("(C) 2021 RGV")]
+
+
