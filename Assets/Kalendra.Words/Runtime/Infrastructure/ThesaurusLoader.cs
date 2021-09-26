7246c19627a816d8f2f22374d041b381df17f3f2
RGV <ricardo.guzman.velasco@outlook.com>
2021-09-26 09:46:59 +0200

    ThesaurusLoader test to ensure random words are slaved by seed

--- Assets/Kalendra.Words/Runtime/Infrastructure/ThesaurusLoader.cs
+++ Assets/Kalendra.Words/Runtime/Infrastructure/ThesaurusLoader.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using JetBrains.Annotations;
+using Kalendra.Commons.Runtime.Domain.Services;
 using Kalendra.Words.Runtime.Domain;
 using Kalendra.Words.Runtime.Infrastructure.Details;
 using UnityEngine;
@@ -8,13 +10,17 @@ namespace Kalendra.Words.Runtime.Infrastructure
 {
     public class ThesaurusLoader
     {
-        public Thesaurus Load(string language)
+        public Thesaurus Load(string language) => Load(language, null!);
+
+        public Thesaurus Load(string language, [NotNull] IRandomService random)
         {
             var file = Resources.Load<TextAsset>(language);
             var reader = new ByteReader(file, ',');
             
-            var words = reader.ReadCSV();
-            return new Thesaurus(words.Where(w => !string.IsNullOrWhiteSpace(w)));
+            var rawWords = reader.ReadCSV();
+            var words = rawWords.Where(w => !string.IsNullOrWhiteSpace(w));
+            
+            return new Thesaurus(words, random);
         }
     }
 }
--- Assets/Kalendra.Words/Tests/Runtime/ThesaurusLoaderTests.cs
+++ Assets/Kalendra.Words/Tests/Runtime/ThesaurusLoaderTests.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections.Generic;
 using FluentAssertions;
+using FluentAssertions.Execution;
+using Kalendra.Commons.Runtime.Domain.Services;
 using Kalendra.Words.Runtime.Infrastructure;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace Kalendra.Words.Tests.Runtime
 {
@@ -41,6 +45,25 @@ namespace Kalendra.Words.Tests.Runtime
             var result = loader.Load(language);
 
             result.HasWord(string.Empty).Should().BeFalse();
+        }
+
+        [Test, Retry(10)]
+        public void TwoThesaurus_LoadedWithSameSeedAndLanguage_ReturnSameWords()
+        {
+            //Arrange
+            var seed = DateTime.Now.GetHashCode();
+            IRandomService Random() => new SystemRandomService { Seed = seed };
+            
+            var loader = new ThesaurusLoader();
+
+            //Act
+            var sut1 = loader.Load("Spanish", Random());
+            var sut2 = loader.Load("Spanish", Random());
+
+            //Assert
+            using(new AssertionScope())
+                for(var _ = 0; _ < 50; _++)
+                    sut1.GetWord().Should().Be(sut2.GetWord());
         }
     }
 }
